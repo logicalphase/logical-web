5a4a3d3a2fb1f8fea526559aa8ca90a1065a63ed
John Teague <john@themesurgeons.com>
2018-08-10 01:12:56 -0500

    Combined template parts in elements

--- src/components/solutions/emergency/ts-emergency.js
+++ src/components/solutions/emergency/ts-emergency.js
@@ -1,14 +1,106 @@
 import { PageViewElement } from '../../page-view-element';
 import { html } from 'lit-html';
 import { SharedStyles } from '../../shared-styles';
-import { EmergencyStyles } from './ts-emergency-css';
 
 class TSEmergency extends PageViewElement {
   _render(props) {
     return html `
       ${SharedStyles}
-      ${EmergencyStyles}
-        <article id="ts-site" class="ts-emergency">
+      <style>
+      :host {
+        display: block;
+        padding: 0px;
+        h1 {
+          margin: 0 0 5px -3px;
+          color: #77909c;
+        }
+        h2 {
+          margin-bottom: 40px;
+        }
+        ul {
+          margin-bottom:24px;
+        }
+      }
+      
+      /* Smaller than 460 */
+
+      .ts-emergency .hero {
+        background: none; 
+        padding: 34px;
+        text-align: center; }
+
+      h3 {
+        padding-top: 20px; }
+
+      @media (min-width: 460px) {
+        #ts-site.ts-emergency {
+          background: #fff url(/images/header/ts-emergency-header.svg) no-repeat;
+          background-size: 35%;
+          background-position: 100% 90px;
+          background-attachment: fixed;
+        }
+
+        #ts-site.ts-emergency .hero {
+          min-height: 270px;
+          padding: 14px;
+          position: relative;
+        }
+
+        .solutions-section-height {
+          min-height:350px;
+        }
+
+        .ts-content-body {
+          margin-bottom: 24px
+        }
+
+        .ts-content-body h2 {
+          margin-bottom: 16px;
+        }
+
+        #ts-site .ts-pad-right-4 {
+          padding-right: 32px;
+        }
+
+        #ts-site.ts-showcase.is-split-left .ts-showcase-image {
+          right: 0;
+        }
+
+        #ts-site .ts-pad-left-38+.ts-showcase-image,
+        #ts-site .ts-pad-right-38+.ts-showcase-image {
+          max-width: 304px;
+        }
+
+        #ts-site.ts-showcase-image {
+          position: relative;
+          text-align: center;
+          top: 0;
+          z-index: 1;
+        }
+
+        p.fine-print:first-child {
+          margin-top: 24px;
+        }
+
+        .fine-print {
+          margin-top: 20px;
+          margin-bottom: 10px;
+          font-size: 12px;
+          padding-left: 24px;
+          line-height: 14px;
+        }
+      }
+      @media (max-width: 800px) {
+        #ts-site.ts-emergency {
+          background-size: 40% !important;
+          background-position: 100% 120px !important; }
+
+        .ts-right {
+          float: none !important;
+          margin: 24px 30px 5px; }
+      } 
+      </style>
+      <article id="ts-site" class="ts-emergency">
         <header class="hero">
           <div class="ts-max-width-standard">
             <h1 class="paper-font-display2 paper-font-light">WordPress 911</h1>
