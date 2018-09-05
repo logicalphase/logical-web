894772017794aca1bb4a04b48b04a475d3a2fa13
John Teague <john@themesurgeons.com>
2018-09-04 19:30:57 -0500

    Moved License to root file. Minor clean up and edits.

--- src/components/ts-article.js
+++ src/components/ts-article.js
@@ -1,13 +1,3 @@
-/**
-@license
-Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-
 import { LitElement, html } from '@polymer/lit-element';
 import { repeat } from 'lit-html/directives/repeat.js';
 import { unsafeHTML } from 'lit-html/directives/unsafe-html.js';
@@ -43,7 +33,7 @@ class TSDetail extends connect(store)(LitElement) {
     
     // Don't render if there is no item.
     if (!_data) {
-      return html `Loading. . .`;
+      return html `<p class="ts-loader">Loading. . .</p>`;
     }
 
     const item = _data;
