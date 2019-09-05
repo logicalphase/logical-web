effd5efc972dfdcbc09eef6717e479cb88a9dab2
John Teague <john@themesurgeons.com>
2019-09-04 19:41:31 -0500

    Updated CSS layout and vector images on care view

--- src/components/ts-care.js
+++ src/components/ts-care.js
@@ -1,15 +1,17 @@
-import { CDN_HOST_URL } from './config';
+import { CDN_HOST_URL } from "./config";
 
-import { html, css, unsafeCSS } from 'lit-element';
-import { PageViewElement } from './page-view-element.js';
-import { updateMetadata } from 'pwa-helpers/metadata.js';
+import { html, css, unsafeCSS } from "lit-element";
+import { PageViewElement } from "./page-view-element.js";
+import { updateMetadata } from "pwa-helpers/metadata.js";
 
-import './ts-sidebar-nav';
+import "./ts-sidebar-nav";
 
-import { SharedStyles } from './ts-style-shared';
-import { TsButtonStyle } from './ts-style-button';
-import { TsElevationStyle } from './ts-style-elevation';
-import { TsTypographyStyle } from './ts-style-typography';
+import { SharedStyles } from "./ts-style-shared";
+import { TsTypographyStyle } from "./ts-style-typography";
+import { TsLayoutStyle } from "./ts-style-layout";
+import { TsGridStyle } from "./ts-style-grid";
+import { TsCard } from "./ts-style-card";
+import { TsButtonStyle } from "./ts-style-button";
 
 const cdnHost = unsafeCSS(CDN_HOST_URL);
 
@@ -17,103 +19,158 @@ class TSCare extends PageViewElement {
   static get styles() {
     return [
       SharedStyles,
-      TsButtonStyle,
-      TsElevationStyle,
       TsTypographyStyle,
+      TsLayoutStyle,
+      TsGridStyle,
+      TsCard,
+      TsButtonStyle,
       css`
       :host {
         display: block;
         padding: 0px;
       }
-        /* Smaller than 460 */
 
+      /* Smaller than 460 */
       .hero {
-        background: var(--app-reverse-text-color) url('https://${cdnHost}/images/header/ts-care-header-opt.svg') no-repeat;
-        background-size: contain;
-        background-position: center center;
+        background: var(--app-reverse-text-color) url('/images/header/ts-care-header-opt.svg') no-repeat;
+        background-size: 424px;
+        background-position: 93% 110px;
       }
-
-      figure.img-right {
-        margin: 44px 0px 44px 0;
+      .ts-section {
+          padding-top: 0px;
       }
-
+      img.ts-content-grid-image, 
+      img.ts-content-grid-image-lower {
+        height: auto;
+        max-width: 100%;
+        width: 100%;
+      }
+      .ts-grid__col--horizontal-center {
+        justify-self: normal;
+      }
       @media (min-width: 460px) {
 
         h3 {
-        padding-top: 20px; }
-
-        figure.img-right {
-          margin-top: 64px;
-          margin-right: 60px;
+          padding-top: 20px; 
+        }
+        img.ts-content-grid-image, 
+        img.ts-content-grid-image-lower {
+          height: auto;
+          max-width: 100%;
+          width: 82%;
+          margin-top: 36px;
+          margin-bottom: 54px;
         }
 
-        .hero {
-          background-position: 90% center;
+        .content-set p {
+          max-width: 600px;
+        }
+
+        section.ts-copy {
+          margin: 0;
+        }
+
+        .ts-grid-row-start {
+          grid-row-start: 1;
+        }
+        .ts-section {
+          padding-top: 40px;
+        }
+        #ts-site .ts-headline4 {
+          margin-right: 56px;
         }
       }
 
       @media (max-width: 800px) {
         .hero {
-          background-position: 90% center !important;
+          background: none;
+          }
+        .ts-content-grid-image-lower {
+          margin-top: 46px;
+          margin-bottom: 36px;
         }
       }   
-    `
+      `
     ];
-  } 
+  }
 
   render() {
     updateMetadata({
       title: 'WordPress Preventive Care Program - HyperPress',
-      description: 'omplete protection and monitoring for WordPress'
+      description: 'Complete protection and monitoring for WordPress'
     });
 
     return html`
-    <article id="ts-site" class="ts-care">
-      <header class="hero">
-        <div class="ts-header-wrapper fade-in">
-          <h1 class="paper-font-display2 paper-font-light">WordPress Preventive Care</h1>
-          <h2 class="paper-font-title paper-font-light">Complete protection and monitoring for WordPress</h2>
-          <div class="center-button">
-            <a href="/contact/" class="button button-large button-logo ts-button" track-type="navigateTo" track-name="home" track-metadata-position="banner">Contact a WordPress Pro</a>
-          </div>
-        </div>
-      </header>
-      <div class="ts-content-wrapper">
-        <section class="content full-bleed-section ts-pad-top-6 ts-pad-bottom-12">
-          <div class="columns">
-            <main class="main delayed-fade-in">
-              <div class="ts-content-grid-box">
-                <figure class="img-right">
-                  <img src="https://${cdnHost}/images/content/ts-care-updates-200x200-opt.svg" alt="WordPress Backups, Updates, Security, Monitoring" />
-                </figure>
-                <h3 class="text-headline">Security, Updates, Backups, and Monitoring, Oh my!</h3>
-                <p>Keeping WordPress, themes, and plugins up-to-date can make anyone anxious. But not updating can open up security vulnerabilities and cause malfunctions. We can take care of the entire process for you, so you can focus on your customers.</p>
-                <p>With our WordPress Preventive Care program you'll rest easy knowing that your site is maticulously maintained and always monitored. Our WordPress Preventive Care Program includes 12 months of:</p>
-                <ul class="body-checks">
-                  <li>Daily encrypted backups of site files and data</li>
-                  <li>Backup stored on our encrypted storage</li>
-                  <li>Backup stored on your cloud provider*</li>
-                  <li>Pretested updates for WordPress core</li>
-                  <li>Pretested updates for installed plugins</li>
-                  <li>Pretested updates for installed themes</li>
-                  <li>Updates for premium themes and plugins**</li>
-                  <li>24 hour site health and security monitoring</li>
-                  <li>Advanced security and spam protection</li>
-                  <li>No contract, optional annual renewal</li>
-                </ul>
-                <a class="button button-secondary-cta" data-customizer="pagespeed-order-form" href="/contact/">Order WordPress Preventive Care</a>
-                <p class="fine-print"> * Dropbox, Google Cloud, Google Drive, Amazon S3, Microsoft Azure</p>
-                <p class="fine-print">** Some Premium themes and plugins not eligible for auto-updating not included.</p>
+      <div class="hypersite-main-content clearfix">
+        <article id="ts-site" class="ts-design">
+          <header class="ts-hero hero">
+            <div class="ts-grid">
+              <div class="ts-grid__column is-7 is-6__large is-1__large--offset">
+                <header class="ts-grid__column is-7 is-6__large is-1__large--offset">
+                  <div class="fade-in content-set">
+                    <h1 class="ts-section-header__eyebrow ts-eyebrow">WordPress Preventive Care</h1>
+                    <h2 class="ts-display3">Meticulous Care for WordPress</h2>
+                    <p class="ts-headline4 ts-why-google__intro-text">We take care of maintaining your site so you can focus on your customers.</p>
+                    <div class="ts-section-header__link">
+                      <a 
+                        href="/contact/" 
+                        class="ts-button ts-button--primary" 
+                        track-type="navigateTo" 
+                        track-name="preventiveCare"
+                        track-metadata-position="banner">Let's maintain your site</a>
+                    </div>
+                  </div>
+                </header>
               </div>
-            </main>
-            <aside class="sidebar">
-              <ts-sidebar-nav></ts-sidebar-nav>
-            </aside> 
+              <div class="cloud-grid__col is-5"></div>
+            </div>
+          </header>
+          <div class="ts-section ts-section--border">
+            <div class="ts-section__spacer">
+              <section class="ts-copy">
+                <div class="ts-grid--alternate ts-copy__inner ts-copy--60-40 ts-grid-row-start">
+                  <div class="ts-grid__col ts-copy--flipped ts-grid__col--horizontal-center ts-copy__img is-4 is-8--offset is-7__large--offset ts-grid-row-start">
+                    <img class="ts-content-grid-image" src="/images/content/ts-care-updates-200x200-opt.svg" alt="WordPress Backups, Updates, Security, Monitoring" />
+                  </div>
+                  <div class="ts-grid__col is-1__large--offset ts-copy__text is-7 is-6__large ts-grid-row-start">
+                    <h3 class="ts-headline3 ts-element-align" id="engaging-experiences-regardless-screen-size">Security, Updates, Backups and more</h3>
+                    <p>Keeping WordPress, themes, and plugins up-to-date can make anyone anxious. But not updating can open up security vulnerabilities and cause malfunctions.</p>
+                    <p>With our WordPress Preventive Care program you'll rest easy knowing your site is maticulously maintained and monitored 24-7 by WordPress experts. Our <strong>WordPress Preventive Care Program</strong> includes 12 months of:</p>
+                    <ul class="body-checks">
+                      <li>Daily encrypted backups of site files and data</li>
+                      <li>Backup stored on our encrypted storage</li>
+                      <li>Backup stored on your cloud provider*</li>
+                      <li>Pretested updates for WordPress core</li>
+                      <li>Pretested updates for installed plugins</li>
+                      <li>Pretested updates for installed themes</li>
+                      <li>Updates for premium themes and plugins**</li>
+                      <li>24 hour site health and security monitoring</li>
+                      <li>Advanced security and spam protection</li>
+                      <li>No contract, optional annual renewal</li>
+                    </ul>
+                    <div class="ts-content-header__link ts-element-align">
+                      <a 
+                        href="/contact/" 
+                        class="ts-button ts-button--primary ts-content-button ts-element-display" 
+                        track-type="navigateTo" 
+                        track-name="preventiveCare"
+                        track-metadata-position="banner">Let's get started</a>
+                    </div>                  
+                  </div>
+                </div>
+              </section>
+            </div>
           </div>
-        </section>
+          <div class="ts-section ts-section--border">
+            <div class="ts-section__header ts-text-center ts-grid">
+              <div class="ts-grid__col is-12 is-10__large is-1__large--offset">
+                <h2 class="ts-headline3" id="features">HyperPress Features</h2>
+              </div>
+            </div>
+          </div>
+        </article>
       </div>
-    </article> 
-  `;
+    `;
   }
 }
-window.customElements.define('ts-care', TSCare);
+window.customElements.define("ts-care", TSCare);
